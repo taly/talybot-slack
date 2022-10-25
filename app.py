615a6a0f524d936db03d4868b9b85a89924a7868
Taly <tafitaf+1@gmail.com>
2022-10-25 14:41:24 +0300

    Response type is now in channel, not ephemeral

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Hello from Talybot! You are user {} in channel {} and you just said '{}'.".format(request.form.get('user_name'), request.form.get('channel_name'), request.form.get('text'))
+    return {
+        "text": "Hello from Talybot! You are user {} in channel {} and you just said '{}'.".format(request.form.get('user_name'), request.form.get('channel_name'), request.form.get('text')),
+        "response_type": "in_channel"
+    }
