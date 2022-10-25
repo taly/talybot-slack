d93b8b4c98eccb1240fd71208ababcd45ad20bde
Taly <tafitaf+1@gmail.com>
2022-10-25 13:25:46 +0300

    Added basic interactive functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Hello Talybot!"
+    return "Hello from Talybot! You are user {} in channel {} and you just said '{}'.".format(request.form.get('user_name'), request.form.get('channel_name'), request.form.get('text'))
